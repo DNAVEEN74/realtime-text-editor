b9edfac8087455b8c7ba1e87e43610a3d786978e
unknown <naveendurgam74@gmail.com>
2024-10-11 12:36:07 +0530

    updated vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vercel from '@vercel/analytics/plugin';
 
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react()],
 });
